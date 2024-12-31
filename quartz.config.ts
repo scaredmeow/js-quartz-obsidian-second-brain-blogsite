5230aae79416f866a8e08949d0d2eb52874867c7
Neil Riego <neilchristianriego3@gmail.com>
2024-12-31 22:57:50 +0800

    fix(config): disable social image generation

--- quartz.config.ts
+++ quartz.config.ts
@@ -19,7 +19,7 @@ const config: QuartzConfig = {
     baseUrl: "brain.neilriego.me",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
-    generateSocialImages: true,
+    generateSocialImages: false,
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
